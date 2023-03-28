7a84c5e91047c85bdd0b9b35a6fea1fc49306d81
ils-harshn <h.narwariya@thesynapses.com>
2023-03-28 18:09:30 +0530

    Responsive Navbar and Sidebar

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,16 @@
 // dashboard js
 
-function hideSidebar() {
-    let sidebar = document.getElementsByClassName("sidebar")[0];
-    sidebar.classList.add("hide");
-    let container = document.getElementsByClassName("container")[0];
-    container.style.marginLeft = 0;
-}
-
 function showSidebar() {
     let sidebar = document.getElementsByClassName("sidebar")[0];
-    sidebar.classList.remove("hide");
-    sidebar.style.zIndex = 1;
+    sidebar.classList.add("active");
+}
+
+function hideSidebar() {
+    let sidebar = document.getElementsByClassName("sidebar")[0];
+    sidebar.classList.remove("active");
+}
+
+
+function toggleSidebar() {
+    showSidebar()
 }
