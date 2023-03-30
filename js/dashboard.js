2a72245cc39bcd360705faf8c272850bfb26856c
ils-harshn <h.narwariya@thesynapses.com>
2023-03-30 12:06:53 +0530

    Added Dropdown

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,7 @@
 function showSidebar() {
     let sidebar = document.getElementsByClassName("sidebar")[0];
     sidebar.classList.add("active");
+    hideDropdown()
 }
 
 function hideSidebar() {
@@ -11,6 +12,27 @@ function hideSidebar() {
 }
 
 
-function toggleSidebar() {
-    showSidebar()
+function showDropdown() {
+    let sidebar = document.getElementsByClassName("drop-down")[0];
+    sidebar.classList.add("active");
+}
+
+function hideDropdown() {
+    let sidebar = document.getElementsByClassName("drop-down")[0];
+    sidebar.classList.remove("active");
+}
+
+function toggleDropdown() {
+    let dropdown = document.getElementsByClassName("drop-down")[0];
+    if (dropdown.classList.contains("active")) {
+        dropdown.classList.remove("active");
+    } else {
+        dropdown.classList.add("active");
+        hideSidebar()
+    }
+}
+
+function hideAllElements() {
+    hideDropdown()
+    hideSidebar()
 }
