a7ddc07939622f24d2cdbbdbae6cc59affd00ef8
ils-harshn <h.narwariya@thesynapses.com>
2023-04-05 17:12:27 +0530

    Added Bar charts for both Roster and Techers

--- charts/barchart.js
+++ charts/barchart.js
@@ -1,19 +1,31 @@
-var xValues = ["School_Name_01", "School_Name_02", "School_Name_03", "School_Name_04"];
-var barColors = "#F9AC32";
+var xValuesRoster = ["School_Name_01", "School_Name_02", "School_Name_03", "School_Name_04"];
+var xValuesTeacher = ["Teacher_Name_01", "Teacher_Name_02", "Teacher_Name_03", "Teacher_Name_04"];
+var barColorsRoster = "#F9AC32";
+var barColorsTeacher = "red";
 
-new Chart("bar-plot", {
+var rosterData = [
+  [150, 210, 350, 250],
+  [50, 90, 50, 60],
+]
+
+var teacherData = [
+  [190, 310, 50, 25],
+  [10, 20, 20, 60],
+]
+
+new Chart("roster-plot", {
   type: "bar",
   data: {
-    labels: xValues,
+    labels: xValuesRoster,
     datasets: [{
       label: "Active",
-      backgroundColor: barColors,
-      data: [150, 210, 350, 250],
+      backgroundColor: barColorsRoster,
+      data: rosterData[0],
       barThickness: 18
     }, {
       label: "Inactive",
       backgroundColor: "#EEEEEE",
-      data: [50, 90, 50, 60],
+      data: rosterData[1],
       barThickness: 18
     }]
   },
@@ -59,3 +71,98 @@ new Chart("bar-plot", {
     aspectRatio: 2
   },
 });
+
+new Chart("teacher-plot", {
+  type: "bar",
+  data: {
+    labels: xValuesTeacher,
+    datasets: [{
+      label: "Active",
+      backgroundColor: barColorsTeacher,
+      data: teacherData[0],
+      barThickness: 18
+    }, {
+      label: "Inactive",
+      backgroundColor: "#EEEEEE",
+      data: teacherData[1],
+      barThickness: 18
+    }]
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        display: false,
+      },
+      legend: {
+        display: true,
+        position: 'right',
+        align: "start",
+        labels: {
+          usePointStyle: true,
+          pointStyle: "circle",
+          fontColor: '#444B48',
+          boxWidth: 6,
+          boxHeight: 6,
+          borderRadius: "50",
+        }
+      },
+    },
+
+    scales: {
+      x: {
+        stacked: true,
+        grid: { display: false },
+      },
+
+      y: {
+        stacked: true,
+        border: {
+          display: false
+        },
+        ticks: {
+          stepSize: 100,   // forces step size to be 50 units
+          min: 0,
+          max: 400
+        },
+      },
+    },
+    responsive: true,
+    aspectRatio: 2
+  },
+});
+
+
+function createRosterPlot() {
+  document.getElementById("teacher-plot").style.display = "none";
+  let ctx = document.getElementById("roster-plot");
+  ctx.style.display = "block";
+}
+
+
+function createTeacherPlot() {
+  document.getElementById("roster-plot").style.display = "none";
+  let ctx = document.getElementById("teacher-plot");
+  ctx.style.display = "block";
+}
+
+
+function showRosterPlot() {
+  let [rosterButton, teacherButton] = document.getElementsByClassName("bar-tab");
+  rosterButton.classList.add("active");
+  rosterButton.classList.remove("deactive");
+  teacherButton.classList.add("deactive");
+  teacherButton.classList.remove("active");
+  createRosterPlot();
+}
+
+function showTeacherPlot() {
+  let [rosterButton, teacherButton] = document.getElementsByClassName("bar-tab");
+  rosterButton.classList.add("deactive");
+  rosterButton.classList.remove("active");
+  teacherButton.classList.add("active");
+  teacherButton.classList.remove("deactive");
+  createTeacherPlot();
+}
+
+
+showRosterPlot();
