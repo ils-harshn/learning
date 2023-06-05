17f20ddd6fdb45a267bbfa3f3a71aac8d7d00c4a
ils-harshn <h.narwariya@thesynapses.com>
2023-06-05 14:44:24 +0530

    app commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import files from "./files.json";
+
+const Directory = ({ files }) => {
+  const [isExpanded, toggleExpanded] = useState(false);
+  if (files.type === "file") {
+    return (
+      <li className='file-name'>{files.name}</li>
+    )
+  }
+  return (
+    <div className='folder'>
+      <input type='checkbox' checked={isExpanded}></input>
+      <h2 className='folder-title' onClick={() => toggleExpanded(!isExpanded)}>{files.name}</h2>
+      <ul>
+        {
+          isExpanded && files.items.map((item, index) => <Directory files={item} key={index} />)
+        }
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory files={files} />
   );
 }
 
