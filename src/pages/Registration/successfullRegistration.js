23eab3b80b058355c3370d6c8c4ed4b7197e9d92
ils-harshn <h.narwariya@thesynapses.com>
2023-06-15 13:18:23 +0530

    Warning fixed rel=noreferrer

--- src/pages/Registration/successfullRegistration.js
+++ src/pages/Registration/successfullRegistration.js
@@ -3,7 +3,7 @@ const SuccessfullRegistration = ({ user }) => {
         <>
             <h3>Registration SuccessFull</h3>
             <p>A email have been sent to you for email verification at {user.email}</p>
-            <a href="https://mail.google.com/" target="_blank">Open your indox</a>
+            <a href="https://mail.google.com/" target="_blank" rel="noreferrer">Open your indox</a>
         </>
     )
 }
