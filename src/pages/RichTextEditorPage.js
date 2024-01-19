50db1a70fc265462deb016a74927c7b3b4543462
ils-harshn <h.narwariya@thesynapses.com>
2024-01-19 13:04:11 +0530

    removed strict mode

--- src/pages/RichTextEditorPage.js
+++ src/pages/RichTextEditorPage.js
@@ -6,24 +6,24 @@ const { useState } = require("react");
 const { Editor } = require("react-draft-wysiwyg");
 
 const RichTextEditorPage = () => {
-  const [richText, setRichText] = useState(() => EditorState.createEmpty());
+  const [editorText, setEditorText] = useState(() => EditorState.createEmpty());
 
   const handleEditorChange = (state) => {
-    setRichText(state);
+    setEditorText(state);
   };
 
   return (
     <div className="rich-text-editor-page">
       <Editor
-        editorState={richText}
+        editorState={editorText}
         onEditorStateChange={handleEditorChange}
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
       />
-      <textarea
-        value={draftToHtml(convertToRaw(richText.getCurrentContent()))}
-      ></textarea>
+      <div className="rich-text-container">
+        {draftToHtml(convertToRaw(editorText.getCurrentContent()))}
+      </div>
     </div>
   );
 };
