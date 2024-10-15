1285d26ad8ec445a7cdb228ef44201147b9bdeac
ils-harshn <127953102+ils-harshn@users.noreply.github.com>
2024-10-15 19:06:52 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,181 @@
+export default [
+  {
+    id: "30",
+    title: "Mystic Mountains",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2023/08/18/176533-855914675_large.mp4",
+  },
+  {
+    id: "29",
+    title: "Sunset Peaks",
+    videoUrl: "https://cdn.pixabay.com/video/2024/06/17/217092_large.mp4",
+  },
+  {
+    id: "28",
+    title: "Whispering Pines",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2018/12/26/20270-308335575_large.mp4",
+  },
+  {
+    id: "27",
+    title: "Eternal Horizons",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2022/02/11/107431-678258669_large.mp4",
+  },
+  {
+    id: "26",
+    title: "Skyline Adventures",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2020/08/07/46637-448480646_large.mp4",
+  },
+  {
+    id: "25",
+    title: "Dreamy Valleys",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2023/03/01/152798-803733100_large.mp4",
+  },
+  {
+    id: "24",
+    title: "Serene Heights",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2022/09/19/131824-751934493_large.mp4",
+  },
+  {
+    id: "23",
+    title: "Wild Ridge",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2022/10/04/133507-756991150_large.mp4",
+  },
+  {
+    id: "22",
+    title: "Mountain Spirit",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2020/02/09/32132-390688056_large.mp4",
+  },
+  {
+    id: "21",
+    title: "Silent Peaks",
+    videoUrl:
+      "https://cdn.pixabay.com/video/2021/04/13/70962-536644240_large.mp4",
+  },
+  {
+    id: "20",
+    title: "Beyond the Clouds",
+    videoUrl:
+      "https://media.istockphoto.com/id/2155595790/video/happy-girl-in-mountains-raising-arms-with-walk-at-tandi-lahaul-india-travel-and-holidays.mp4?s=mp4-640x640-is&k=20&c=_k35UPI9qKENiSBRBKm9PpvSQerS4a2fp5blHHjymbo=",
+  },
+  {
+    id: "19",
+    title: "Wanderer's Peak",
+    videoUrl:
+      "https://media.istockphoto.com/id/1499738731/video/mature-business-woman-stock-video.mp4?s=mp4-640x640-is&k=20&c=AzBjIysyQXH48eNZK0bHwW13tsX2FYk3txJMF-h60V0=",
+  },
+  {
+    id: "1",
+    title: "This World is Rapid",
+    videoUrl:
+      "https://videos.pexels.com/video-files/6646588/6646588-hd_1080_1920_30fps.mp4",
+  },
+  {
+    id: "2",
+    title: "Summit of Dreams",
+    videoUrl:
+      "https://videos.pexels.com/video-files/28914112/12514809_1440_2560_30fps.mp4",
+  },
+  {
+    id: "3",
+    title: "The Mountain Melody",
+    videoUrl:
+      "https://videos.pexels.com/video-files/8432041/8432041-uhd_1440_2732_25fps.mp4",
+  },
+  {
+    id: "4",
+    title: "Whispers in the Hills",
+    videoUrl:
+      "https://videos.pexels.com/video-files/6248207/6248207-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "5",
+    title: "Nature's Heartbeat",
+    videoUrl:
+      "https://videos.pexels.com/video-files/6923030/6923030-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "6",
+    title: "Serene Escape",
+    videoUrl:
+      "https://videos.pexels.com/video-files/6646595/6646595-hd_1080_1920_30fps.mp4",
+  },
+  {
+    id: "7",
+    title: "Mystic Valley",
+    videoUrl:
+      "https://videos.pexels.com/video-files/6000223/6000223-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "8",
+    title: "Lost Horizons",
+    videoUrl:
+      "https://videos.pexels.com/video-files/7279832/7279832-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "9",
+    title: "Winds of Freedom",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5244400/5244400-uhd_1440_2560_25fps.mp4",
+  },
+  {
+    id: "10",
+    title: "Echoes of the Wild",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5744830/5744830-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "11",
+    title: "Peak Reflections",
+    videoUrl:
+      "https://videos.pexels.com/video-files/7214688/7214688-hd_720_1280_25fps.mp4",
+  },
+  {
+    id: "12",
+    title: "Distant Heights",
+    videoUrl:
+      "https://videos.pexels.com/video-files/7987249/7987249-uhd_1440_2560_30fps.mp4",
+  },
+  {
+    id: "13",
+    title: "Endless Views",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5274888/5274888-uhd_1440_2732_25fps.mp4",
+  },
+  {
+    id: "14",
+    title: "The Quiet Ascent",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5229780/5229780-uhd_1440_2560_24fps.mp4",
+  },
+  {
+    id: "15",
+    title: "Sunrise Summit",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5935106/5935106-hd_1080_1920_25fps.mp4",
+  },
+  {
+    id: "16",
+    title: "Majestic Hills",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5730333/5730333-uhd_1440_2732_25fps.mp4",
+  },
+  {
+    id: "17",
+    title: "Tranquil Ridge",
+    videoUrl:
+      "https://videos.pexels.com/video-files/5275698/5275698-uhd_1440_2732_25fps.mp4",
+  },
+  {
+    id: "18",
+    title: "Timeless Peaks",
+    videoUrl:
+      "https://videos.pexels.com/video-files/7299360/7299360-uhd_1440_2560_30fps.mp4",
+  },
+];
